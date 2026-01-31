5df17484724c27793457ba15ea49d055aad060e0
Fariz-Isg <fariz.isgandarli@gmail.com>
2026-01-31 22:18:56 +0400

    g

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module for cat_arrays function
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    Concatenates two arrays
+    Args:
+        arr1: first array
+        arr2: second array
+    Returns:
+        New list with concatenated arrays
+    """
+    return arr1 + arr2
