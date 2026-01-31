31c183910b62511e7ec90bef202536638e075ef7
Fariz-Isg <fariz.isgandarli@gmail.com>
2026-01-31 22:28:57 +0400

    l

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module for np_transpose function
+"""
+
+
+def np_transpose(matrix):
+    """
+    Transposes matrix
+    Args:
+        matrix: numpy.ndarray
+    Returns:
+        new numpy.ndarray
+    """
+    return matrix.transpose()
