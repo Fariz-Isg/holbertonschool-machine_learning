d0b1dd6a8b67b9a246c462a9550c8b07c1819f4d
Fariz-Isg <fariz.isgandarli@gmail.com>
2026-01-31 22:27:03 +0400

    k

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module for np_shape function
+"""
+
+
+def np_shape(matrix):
+    """
+    Calculates the shape of a numpy.ndarray
+    Args:
+        matrix: the matrix to calculate the shape of
+    Returns:
+        tuple of integers representing the shape
+    """
+    return matrix.shape
