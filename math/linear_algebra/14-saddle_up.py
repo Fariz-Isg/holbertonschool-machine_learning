c4e055f976a456e7e4e2ba439edd36e179eedc36
Fariz-Isg <fariz.isgandarli@gmail.com>
2026-01-31 22:34:12 +0400

    o

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module for np_matmul function
+"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    Performs matrix multiplication
+    Args:
+        mat1: first matrix
+        mat2: second matrix
+    Returns:
+        new numpy.ndarray
+    """
+    return np.matmul(mat1, mat2)
