3d92d4a1a8c6fec1de504f92f43db93b55d07ab2
Fariz-Isg <fariz.isgandarli@gmail.com>
2026-03-14 16:59:06 +0400

    b

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Neuron module"""
+
+import numpy as np
+
+
+class Neuron:
+    """Neuron class"""
+
+    def __init__(self, nx):
+        """Constructor"""
+        if type(nx) is not int:
+            raise TypeError("nx must be a integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter for W"""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter for b"""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter for A"""
+        return self.__A
