3b84f68d1d2525034c44711f364efe26dbdb7676
Fariz-Isg <fariz.isgandarli@gmail.com>
2026-03-14 17:03:55 +0400

    f

---
+++ supervised_learning/classification/5-neuron.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+"""Neuron module"""
+
+import numpy as np
+
+
+class Neuron:
+    """Neuron class"""
+
+    def __init__(self, nx):
+        """Constructor"""
+        if type(nx) is not int:
+            raise TypeError("nx must be a integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter for W"""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter for b"""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter for A"""
+        return self.__A
+
+    def forward_prop(self, X):
+        """
+        Calculates the forward propagation of the neuron
+        Args:
+            X: numpy.ndarray with shape (nx, m) that contains the input data
+        Returns:
+            the private attribute __A
+        """
+        z = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-z))
+        return self.__A
+
+    def cost(self, Y, A):
+        """
+        Calculates the cost of the model using logistic regression
+        Args:
+            Y: numpy.ndarray with shape (1, m) that contains the correct labels
+            A: numpy.ndarray with shape (1, m) containing the activated output
+        Returns:
+            the cost
+        """
+        m = Y.shape[1]
+        loss = - (Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        cost = (1 / m) * np.sum(loss)
+        return cost
+
+    def evaluate(self, X, Y):
+        """
+        Evaluates the neuron's predictions
+        Args:
+            X: numpy.ndarray with shape (nx, m) that contains the input data
+            Y: numpy.ndarray with shape (1, m) that contains the correct labels
+        Returns:
+            the neuron's prediction and the cost of the network
+        """
+        A = self.forward_prop(X)
+        cost = self.cost(Y, A)
+        pred = np.where(A >= 0.5, 1, 0)
+        return pred, cost
+
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+        """
+        Calculates one pass of gradient descent on the neuron
+        Args:
+            X: numpy.ndarray with shape (nx, m) that contains the input data
+            Y: numpy.ndarray with shape (1, m) that contains the correct labels
+            A: numpy.ndarray with shape (1, m) containing the activated output
+            alpha: learning rate
+        Returns:
+            None (Updates the private attributes __W and __b)
+        """
+        m = Y.shape[1]
+        dz = A - Y
+        dw = (1 / m) * np.matmul(X, dz.T).T
+        db = (1 / m) * np.sum(dz)
+
+        self.__W = self.__W - (alpha * dw)
+        self.__b = self.__b - (alpha * db)
