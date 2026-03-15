1f50f869cd311317cde868ae097b0a1b65d17bca
Fariz-Isg <fariz.isgandarli@gmail.com>
2026-03-15 20:42:56 +0400

    h

---
+++ supervised_learning/optimization/7-RMSProp.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""RMSProp module"""
+import numpy as np
+
+
+def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
+    """Updates a variable using the RMSProp optimization algorithm"""
+    s_new = beta2 * s + (1 - beta2) * (grad ** 2)
+    var_new = var - alpha * grad / (np.sqrt(s_new) + epsilon)
+    return var_new, s_new
