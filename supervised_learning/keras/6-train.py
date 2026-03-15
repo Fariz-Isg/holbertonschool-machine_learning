876f3ee0575e4311483681d22d25234d5b68eec1
Fariz-Isg <fariz.isgandarli@gmail.com>
2026-03-15 20:21:21 +0400

    g

---
+++ supervised_learning/keras/6-train.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Train module"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False,
+                patience=0, verbose=True, shuffle=False):
+    """Trains a model using mini-batch gradient descent"""
+    callbacks = []
+    if early_stopping and validation_data:
+        callbacks.append(K.callbacks.EarlyStopping(
+            monitor='val_loss',
+            patience=patience
+        ))
+
+    history = network.fit(
+        x=data,
+        y=labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        shuffle=shuffle,
+        validation_data=validation_data,
+        callbacks=callbacks
+    )
+    return history
