e600a67b28cb136901bacdc79f31a6f17b3f6b50
Fariz-Isg <fariz.isgandarli@gmail.com>
2026-03-15 20:41:57 +0400

    g

---
+++ supervised_learning/optimization/6-momentum.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Momentum Upgrade module"""
+import tensorflow as tf
+
+
+def create_momentum_op(alpha, beta1):
+    """
+    Sets up the gradient descent with momentum optimization algorithm
+    in TensorFlow
+    """
+    return tf.keras.optimizers.SGD(learning_rate=alpha, momentum=beta1)
