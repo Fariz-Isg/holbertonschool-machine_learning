5e0d8bd58f8c20b173b0164e2301da70a75917a7
Fariz-Isg <fariz.isgandarli@gmail.com>
2026-03-15 20:27:16 +0400

    l

---
+++ supervised_learning/keras/11-config.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Config module"""
+import tensorflow.keras as K
+
+
+def save_config(network, filename):
+    """Saves a model's configuration in JSON format"""
+    config = network.to_json()
+    with open(filename, 'w') as f:
+        f.write(config)
+    return None
+
+
+def load_config(filename):
+    """Loads a model with a specific configuration"""
+    with open(filename, 'r') as f:
+        config = f.read()
+    return K.models.model_from_json(config)
