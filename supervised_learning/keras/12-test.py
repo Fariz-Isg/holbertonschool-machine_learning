9fe574ba71c8071c43ed814f75c17aa38b638a4b
Fariz-Isg <fariz.isgandarli@gmail.com>
2026-03-15 20:27:49 +0400

    m

---
+++ supervised_learning/keras/12-test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Test module"""
+import tensorflow.keras as K
+
+
+def test_model(network, data, labels, verbose=True):
+    """Tests a neural network"""
+    return network.evaluate(x=data, y=labels, verbose=verbose)
