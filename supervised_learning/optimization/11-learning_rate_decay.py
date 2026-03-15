86960fbbecdcde30a6a8b7b02f831414c95df397
Fariz-Isg <fariz.isgandarli@gmail.com>
2026-03-15 20:49:20 +0400

    l

---
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Learning Rate Decay module"""
+
+
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """Updates the learning rate using inverse time decay in numpy"""
+    alpha = alpha / (1 + decay_rate * (global_step // decay_step))
+    return alpha
