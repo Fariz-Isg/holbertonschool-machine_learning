df1aa177478efe8af08fba053d67c0ae41fcf2c6
Fariz-Isg <fariz.isgandarli@gmail.com>
2026-03-15 20:40:20 +0400

    e

---
+++ supervised_learning/optimization/4-moving_average.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Moving average module"""
+
+
+def moving_average(data, beta):
+    """Calculates the weighted moving average of a data set"""
+    v = 0
+    moving_averages = []
+    for i in range(len(data)):
+        v = beta * v + (1 - beta) * data[i]
+        bias_correction = 1 - (beta ** (i + 1))
+        moving_averages.append(v / bias_correction)
+    return moving_averages
