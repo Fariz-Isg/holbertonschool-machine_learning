2564d8a8899e6c9ef5c1d95ba3fbb4cb9166fdc9
Fariz-Isg <fariz.isgandarli@gmail.com>
2026-04-03 17:18:37 +0400

    h

---
+++ supervised_learning/regularization/7-early_stopping.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Early Stopping module"""
+
+
+def early_stopping(cost, opt_cost, threshold, patience, count):
+    """Determines if gradient descent should stop early"""
+    if (opt_cost - cost) > threshold:
+        count = 0
+    else:
+        count += 1
+
+    if count >= patience:
+        return True, count
+    else:
+        return False, count
