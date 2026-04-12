a031f0b33b4b50ee38cf52d7f5afbba180d6573f
Fariz-Isg <fariz.isgandarli@gmail.com>
2026-04-12 14:15:34 +0400

    a

---
+++ pipeline/data_augmentation/0-flip.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module to flip an image."""
+import tensorflow as tf
+
+
+def flip_image(image):
+    """Flips an image horizontally."""
+    return tf.image.flip_left_right(image)
