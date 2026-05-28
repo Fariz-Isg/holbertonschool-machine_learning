1d6039b87b38b7553e03ee331580e8cbd20ce1c2
Fariz-Isg <farizisgenderli05@gmail.com>
2026-05-28 12:44:44 +0400

    f

--- unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
@@ -35,10 +35,12 @@ class BayesianOptimization:
 
     def optimize(self, iterations=100):
         """Optimizes the black-box function."""
+        sampled = []
         for _ in range(iterations):
             X_next, _ = self.acquisition()
-            if np.any(np.abs(X_next[0] - self.gp.X.reshape(-1)) < 1e-6):
+            if any(np.abs(X_next[0] - s) < 1e-6 for s in sampled):
                 break
+            sampled.append(X_next[0])
             Y_next = self.f(X_next)
             self.gp.update(X_next, Y_next)
         if self.minimize:
